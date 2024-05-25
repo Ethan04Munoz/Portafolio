7f67d202002bd060de26eaab67a8a5826853bbae
Your Name <you@example.com>
2024-05-25 17:26:25 -0600

    Parallax ahora recibe un link al sitio y un link al repositorio

--- portafolio/src/componentes/ContenidoParallax.jsx
+++ portafolio/src/componentes/ContenidoParallax.jsx
@@ -5,8 +5,13 @@ function ContenidoParallax(props) {
     return (
         <div className="contenidoParallax">
             <div class="contenido-centrado">
-                <h1>{props.titulo}</h1>
+                <p className="tituloContenido">{props.titulo}</p>
                 <p>{props.texto}</p>
+                <div className="gridEnlacesParallax">
+                    <a className="enlace" href={props.enlaceSitio}>Ver sitio</a>
+                    <div></div>
+                    <a className="enlace" href={props.enlaceRepositorio}>Ver repositorio</a>
+                </div>
             </div>
         </div>
     )
