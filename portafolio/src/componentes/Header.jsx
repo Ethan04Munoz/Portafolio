b773fa189ae6edd8ee2ae8ab11b5cd3e6b64a5fa
Your Name <you@example.com>
2024-05-25 17:26:56 -0600

    En el header, contactar ahora es un enlace

--- portafolio/src/componentes/Header.jsx
+++ portafolio/src/componentes/Header.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import './Header.css';
 
-function Header() {
+function Header(props) {
     return (
         <div className="header">
             <div className="nombre">Ethan</div>
             <div></div>
-            <div className="contacto">Contactar</div>
+            <div className="contacto">
+                <a className="enlace" href={props.enlace}>
+                    Contactar
+                </a>
+            </div>
         </div>
     )
 }
