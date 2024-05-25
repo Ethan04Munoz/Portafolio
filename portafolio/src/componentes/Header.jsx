2058a7718301c6be60d367967020aaf49bd5d847
Your Name <you@example.com>
2024-05-24 21:21:05 -0600

    Primer header

---
+++ portafolio/src/componentes/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Header.css';
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="nombre">Ethan</div>
+            <div></div>
+            <div className="contacto">Contactar</div>
+        </div>
+    )
+}
+
+export default Header;
