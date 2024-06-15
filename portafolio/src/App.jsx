933f15e85212cedeeaa5d4850110a7db23ffc661
Your Name <you@example.com>
2024-06-14 21:29:53 -0600

    Instalación y uso de router

--- portafolio/src/App.jsx
+++ portafolio/src/App.jsx
@@ -1,66 +1,16 @@
-import React from "react";
-import Header from "./componentes/Header";
-import Parallax from "./componentes/Parallax";
-import './App.css'
-import ContenidoParallax from "./componentes/ContenidoParallax";
+import {Route, Routes} from "react-router-dom";
+import * as React from "react";
+import { HashRouter } from "react-router-dom";
+import Main from "./paginas/Main";
 
 function App() {
-    return (
-        <div className="App">
-            <div className="contenerSitio">
-                <Header enlace="#tituloContactar"/>
-                <div className="contenedorTextoCentral">
-                    <h1>Websites. Webapps. Soluciones web.</h1>
-                </div>
-                <Parallax
-                    backgroundImage={'img/webp/buscaminasCut.webp'}
-                    strength={0.8}
-                    blur={10}
-                    darken={20}
-                    id={'primerProyecto'}
-                >
-                    <ContenidoParallax
-                        titulo="Buscaminas"
-                        texto={`Un emocionante buscaminas con una interfaz dinámica construida con React. 
-                        Fusiona desafío, estilo y rendimiento con animaciones CSS cautivadoras, almacenamiento de récords en localStorage, 
-                        Web Workers para un juego fluido y una experiencia configurable y responsive.`}
-                        enlaceRepositorio="https://github.com/Ethan04Munoz/Buscaminas"
-                        enlaceSitio="https://ethan04munoz.github.io/Buscaminas/"
-                    />
-                    <div style={{ height: '500px', borderRadius: '1rem 0 1rem 0' }} />
-                </Parallax>
-                <Parallax
-                    backgroundImage={'img/webp/expenseTrackerCut.webp'}
-                    strength={0.8}
-                    blur={10}
-                    darken={20}
-                    id={'ultimoProyecto'}
-                >
-                    <ContenidoParallax 
-                        titulo="Expense tracker" 
-                        texto={`Un sofisticado gestor de gastos e ingresos creado con React que simplifica el control financiero. 
-                        Permite registrar transacciones con facilidad, marcar entradas como recurrentes para un seguimiento automático mensual. 
-                        Ofrece notificaciones visuales atractivas con toast y vistas analíticas claras con gráficos claros.`}
-                        enlaceRepositorio="https://github.com/Ethan04Munoz/ExpenseTracker"
-                        enlaceSitio="https://ethan04munoz.github.io/ExpenseTracker/"
-                    />
-                    <div style={{ height: '500px' }} />
-                </Parallax>
-                <div className="contenerTexto">
-                    <p>
-                        Desarrollador Full Stack enfocado en React, CSS3, Node.js, Express.js y consumo de APIs. Apasionado por crear interfaces atractivas y backends robustos. Comprometido con buenas prácticas, escalabilidad y soluciones innovadoras que aporten valor.
-                    </p>
-                </div>
-                <div className="contenerTexto">
-                    <div className="tituloContenido" id="tituloContactar">
-                        Contactar
-                    </div>
-                    <p><a className="enlace" href="https://github.com/Ethan04Munoz">Github</a></p>
-                    <p><a className="enlace" href="https://www.linkedin.com/in/ethanmuñoz/">LinkedIn</a></p>
-                </div>
-            </div>
-        </div>
-    );
+  return(
+    <HashRouter>
+      <Routes>
+        <Route exact path="/" element={<Main/>}/>
+      </Routes>
+    </HashRouter>
+  )
 }
 
 export default App;
